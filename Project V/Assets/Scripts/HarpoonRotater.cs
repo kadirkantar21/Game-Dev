9ee3bbbf463a8ad1eca6569f165857b225f69d84
kadirkantar21 <qadirwantar@outlook.com>
2021-10-16 18:55:01 +0300

    Zıpkın Geliştirildi
    Zıpkını yaptım bir kaç sıkıntı var düzelticem onları da

--- Project V/Assets/Scripts/HarpoonRotater.cs
+++ Project V/Assets/Scripts/HarpoonRotater.cs
@@ -7,9 +7,55 @@ public class HarpoonRotater : MonoBehaviour
     private float maxZrotation = 60f;
     private float differenceFromLeft = 30f;
     private float turnSpeed = 60f;
+    bool rotating = true;
+    LineRenderer hookLn;
+    public GameObject hook;
+
+
+    private void Start()
+    {
+        hookLn = hook.GetComponent<LineRenderer>();
+    }
+
     private void Update()
     {
-        float angle = Mathf.PingPong(Time.time * turnSpeed, maxZrotation) - differenceFromLeft;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        
+        if (rotating)
+        {
+            float angle = Mathf.PingPong(Time.time * turnSpeed, maxZrotation) - differenceFromLeft;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+        else
+        {
+            FindObjectOfType<ReleaseHarpoon>().Release();
+            
+        }
+       
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rotating = false;
+            FindObjectOfType<ReleaseHarpoon>().finished = false;
+            FindObjectOfType<ReleaseHarpoon>().timeRemain = 2.4f;
+            hookLn.SetPosition(1, new Vector3(0, -1, 0));
+           
+
+        }
+
+        if (FindObjectOfType<ReleaseHarpoon>().finished)
+        {
+            rotating = true;
+            
+
+
+        }
     }
+
+
+
+
+
+
+
 }
--- Project V/Assets/Scripts/ReleaseHarpoon.cs
+++ Project V/Assets/Scripts/ReleaseHarpoon.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class ReleaseHarpoon : MonoBehaviour
 {
-    public GameObject harpoon;
-    public GameObject harpoonHead;
-    GameObject InsHarpoon;
+    public GameObject hook;
+    public float Hookspeed;
+    LineRenderer hookLn;
+    int hookDir = -1;
+    public bool finished = false;
+    public float timeRemain = 2.4f;
+    
+
+    private void Start()
+    {
+        hookLn = hook.GetComponent<LineRenderer>();
+        
+    }
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        
+    }
+
+
+    public void Release()
+    {
+        Vector3 length = hookLn.GetPosition(1);
+
+        if (length.y <= -7) hookDir = 1;
+        if (length.y >= -.9f) hookDir = -1;
+
+        length.y += Hookspeed * hookDir * Time.deltaTime;
+        hookLn.SetPosition(1, length);
+
+        
+        if (timeRemain > 0)
         {
-            InsHarpoon = Instantiate(harpoonHead, harpoon.transform.position, transform.rotation) as GameObject;
+            timeRemain -= Time.deltaTime;
+            
         }
+        else
+        {
+            finished = true;
+            hookLn.SetPosition(1, new Vector3(0, -1, 0));
+
+
+        }
+
+
+        Debug.Log(finished);
+
+
+        
+
+
+
+
+
+
 
 
     }
